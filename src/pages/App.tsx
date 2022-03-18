9bcff09b705cefb89d352ff8906a7321446ad135
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-03-18 18:01:25 -0300

    styling the project

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './App.module.scss';
+
+function App() {
+  return (
+    <div className={style.AppStyile}>
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
