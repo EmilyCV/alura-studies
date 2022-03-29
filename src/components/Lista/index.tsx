e1492d010bef02474c7f851b92e23b8e78d8327c
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-03-29 17:30:11 -0300

    selecting tasks and starting the timer

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -3,13 +3,18 @@ import style from "./Lista.module.scss";
 import Item from "./Item";
 import { ITarefa } from "../../types/tarefa";
 
-function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+interface IProps {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Lista({ tarefas, selecionaTarefa }: IProps) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudo do dia</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
         ))}
       </ul>
     </aside>
