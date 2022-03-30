449a50d887cfe04c9d4d82c24411215e471bd60b
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-03-30 11:22:51 -0300

    refactoring Class Components

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component<{
+interface IProps {
   type?: "button" | "submit" | "reset" | undefined;
-}> {
-  render() {
-    const { type = "button" } = this.props;
-    return (
-      <button type={type} className={style.botao}>
-        {this.props.children}
-      </button>
-    );
-  }
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+function Botao({ onClick, type, children }: IProps) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  );
 }
 
 export default Botao;
